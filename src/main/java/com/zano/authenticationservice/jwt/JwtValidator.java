673802b17acceed06d782f465cf4fe2c87c960d7
BalaganeshA2k <balapunch367@gmail.com>
2023-05-26 01:02:50 +0530

    Change implementation of isTokenValid in JwtValidator

--- src/main/java/com/zano/authenticationservice/jwt/JwtValidator.java
+++ src/main/java/com/zano/authenticationservice/jwt/JwtValidator.java
@@ -1,23 +1,26 @@
 package com.zano.authenticationservice.jwt;
 
-import lombok.RequiredArgsConstructor;
-
 import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import com.zano.authenticationservice.user.detail.UserDetailRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class JwtValidator {
   private final JwtDetailsExtractor jwtDetailsExtractor;
+  private final UserDetailRepository userDetailRepository;
+
   @Value("${application.security.jwt.expiration}")
   private long jwtExpiration;
 
-  public boolean isTokenValid(String token, UserDetails userDetails) {
+  public boolean isTokenValid(String token) {
     final String username = jwtDetailsExtractor.extractUsername(token);
-    
-    return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    return (userDetailRepository.existsByUserEmail(username)) && !isTokenExpired(token);
   }
 
   private boolean isTokenExpired(String token) {
