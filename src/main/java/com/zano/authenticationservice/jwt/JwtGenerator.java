65b4c8acba2bdea11d982aa89492b28ccc461d33
BalaganeshA2k <balapunch367@gmail.com>
2023-05-26 01:02:56 +0530

    change UserSignInRequest import

--- src/main/java/com/zano/authenticationservice/jwt/JwtGenerator.java
+++ src/main/java/com/zano/authenticationservice/jwt/JwtGenerator.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import com.zano.authenticationservice.user.UserSignInRequest;
+import com.zano.authenticationservice.user.registration.UserSignInRequest;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
