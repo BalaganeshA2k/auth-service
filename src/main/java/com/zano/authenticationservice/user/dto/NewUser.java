d7e6f9ba138f1142cd20e74e4e6c4806eb2fcdba
BalaganeshA2k <balapunch367@gmail.com>
2023-05-26 01:05:31 +0530

    Add Validation annotations to New User

--- src/main/java/com/zano/authenticationservice/user/dto/NewUser.java
+++ src/main/java/com/zano/authenticationservice/user/dto/NewUser.java
@@ -1,4 +1,10 @@
 package com.zano.authenticationservice.user.dto;
 
-public record NewUser(String username, String password) {
+import com.zano.authenticationservice.user.validation.annotation.StrongPassword;
+import com.zano.authenticationservice.user.validation.annotation.UnRegisteredUserName;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record NewUser(@UnRegisteredUserName @NotBlank(message = "username cannot be blank") String username,
+                @StrongPassword @NotBlank(message = "password cannot be blank") String password) {
 }
