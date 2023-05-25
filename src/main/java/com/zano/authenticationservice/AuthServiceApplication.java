480a86a856e140204ebdfc2a64b85e98ebf72d68
BalaganeshA2k <balapunch367@gmail.com>
2023-05-26 01:01:07 +0530

    Rename application to AuthServiceApplication

--- src/main/java/com/zano/authenticationservice/AuthServiceApplication.java
+++ src/main/java/com/zano/authenticationservice/AuthServiceApplication.java
@@ -3,11 +3,13 @@ package com.zano.authenticationservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AuthenticationServiceApplication {
+import lombok.RequiredArgsConstructor;
 
+@SpringBootApplication
+@RequiredArgsConstructor
+public class AuthServiceApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(AuthenticationServiceApplication.class, args);
+		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
 }
