0b5d914420fe1fa3fda1ac61fd1cb740c6e80e62
BalaganeshA2k <balapunch367@gmail.com>
2023-05-26 00:59:58 +0530

    Add UserDetailsService and UserSignInStatus

---
+++ src/main/java/com/zano/authenticationservice/user/UserDetailService.java
@@ -0,0 +1,17 @@
+package com.zano.authenticationservice.user;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailService {
+  private final UserDetailRepository userDetailRepository;
+  
+  public UserSignInStatus signInUser(UserSignInRequest userSignInRequest){
+    var userDetail =userSignInRequest.toUserDetail();
+    userDetailRepository.save(userDetail);
+    return UserSignInStatus.SIGNED_IN;
+  }
+}
---
+++ src/main/java/com/zano/authenticationservice/user/UserSignInStatus.java
@@ -0,0 +1,5 @@
+package com.zano.authenticationservice.user;
+
+public enum UserSignInStatus {
+  SIGNED_IN
+}
