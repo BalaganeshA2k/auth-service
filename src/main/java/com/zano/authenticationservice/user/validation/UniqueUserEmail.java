ec255e84ddb57b4824414bc7b67f6d2646a2d3eb
BalaganeshA2k <balapunch367@gmail.com>
2023-05-26 01:02:02 +0530

    Add UniqueUserEmail Validation and it's constraint validator

---
+++ src/main/java/com/zano/authenticationservice/user/validation/UniqueUserEmail.java
@@ -0,0 +1,22 @@
+package com.zano.authenticationservice.user.validation;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+@Documented
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = UniqueUserEmailValidator.class)
+public @interface UniqueUserEmail {
+    String message() default "User Already Exist";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/com/zano/authenticationservice/user/validation/UniqueUserEmailValidator.java
@@ -0,0 +1,21 @@
+package com.zano.authenticationservice.user.validation;
+
+import org.springframework.stereotype.Component;
+
+import com.zano.authenticationservice.user.UserDetailRepository;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class UniqueUserEmailValidator implements ConstraintValidator<UniqueUserEmail, String> {
+    private final UserDetailRepository userDetailRepository;
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext context) {
+        return !userDetailRepository.existsByUserEmail(email);
+    }
+
+}
