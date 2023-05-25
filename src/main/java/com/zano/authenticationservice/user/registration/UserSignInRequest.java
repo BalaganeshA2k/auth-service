3f8256874325eba59c05eaeb9fb699fc9c176745
BalaganeshA2k <balapunch367@gmail.com>
2023-05-26 01:03:33 +0530

    Add @NotEmpty validation to signInRequest Email

--- src/main/java/com/zano/authenticationservice/user/registration/UserSignInRequest.java
+++ src/main/java/com/zano/authenticationservice/user/registration/UserSignInRequest.java
@@ -3,7 +3,8 @@ package com.zano.authenticationservice.user.registration;
 import com.zano.authenticationservice.user.validation.UniqueUserEmail;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 
 public record UserSignInRequest(
-                @Email(message = "Invalid Email format") @UniqueUserEmail String email) {
+        @Email(message = "Invalid Email format") @UniqueUserEmail @NotEmpty(message = "email should not be empty") String email) {
 }
