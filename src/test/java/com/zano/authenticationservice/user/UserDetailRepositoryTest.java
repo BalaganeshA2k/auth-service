c99070d199777a61bc47e31371ec14a0671198f5
BalaganeshA2k <balapunch367@gmail.com>
2023-05-26 01:00:47 +0530

    Add userDetailRepository unit test

---
+++ src/test/java/com/zano/authenticationservice/user/UserDetailRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.zano.authenticationservice.user;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class UserDetailRepositoryTest {
+    @Autowired
+    private UserDetailRepository userDetailRepository;
+
+    @Test
+    public void existsByUsernameShouldReturnTrueIfUserExists(){
+        UserDetail userDetail = UserDetail.builder()
+            .username("balaganesh.a2k@gmail.com")
+            .password("pass")
+            .authorities(Set.of())
+            .build();
+        userDetailRepository.save(userDetail);
+
+        assertTrue(()->userDetailRepository.existsByUsername("balaganesh.a2k@gmail.com"));
+    }
+    @Test
+    public void existsByUsernameShouldReturnFalseIfUserDoesNotExists(){
+        assertFalse(()->userDetailRepository.existsByUsername("balaganesh.a2k@gmail.com"));
+    }
+}
