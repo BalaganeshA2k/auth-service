86375f67c640d900016dd77f21f6782d97170814
BalaganeshA2k <balapunch367@gmail.com>
2023-05-26 01:00:49 +0530

    fetch userDetails lazy from authority table and make name column unique and non nullable

--- src/main/java/com/zano/authenticationservice/authority/Authority.java
+++ src/main/java/com/zano/authenticationservice/authority/Authority.java
@@ -1,11 +1,12 @@
 package com.zano.authenticationservice.authority;
 
 import static jakarta.persistence.FetchType.LAZY;
-
 import java.util.Set;
-
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.GrantedAuthority;
 import com.zano.authenticationservice.user.UserDetail;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,12 +19,18 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Data @NoArgsConstructor @AllArgsConstructor @Builder 
-public class Authority {
+public class Authority implements GrantedAuthority{
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+  @Column(unique = true,nullable = false)
   private String name;
   @ManyToMany(fetch = LAZY,mappedBy = "authorities")
+  @Lazy
   private Set<UserDetail> userDetails;
-  
+  @Override
+  public String getAuthority() {
+    return this.name;
+  }
+
 }
