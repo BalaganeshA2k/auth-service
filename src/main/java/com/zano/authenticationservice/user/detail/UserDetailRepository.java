6f8261f7726bc2580fb8fe0ad5ea404e32ddd162
BalaganeshA2k <balapunch367@gmail.com>
2023-05-26 01:04:38 +0530

    update: add few methods to UserDetailRepository

--- src/main/java/com/zano/authenticationservice/user/detail/UserDetailRepository.java
+++ src/main/java/com/zano/authenticationservice/user/detail/UserDetailRepository.java
@@ -1,8 +1,13 @@
 package com.zano.authenticationservice.user.detail;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserDetailRepository extends JpaRepository<UserDetail, Long> {
+  Optional<UserDetail> findOneByUserEmail(String userEmail);
 
   boolean existsByUserEmail(String userEmail);
+
+  Optional<Long> findIdByUserEmail(String userEmail);
 }
