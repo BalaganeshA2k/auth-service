7660a7151216c4b71bbc80021e9ed0adc652658e
BalaganeshA2k <balapunch367@gmail.com>
2023-05-26 01:00:02 +0530

    Update UserController to use UserDetailService

--- src/main/java/com/zano/authenticationservice/user/UserController.java
+++ src/main/java/com/zano/authenticationservice/user/UserController.java
@@ -6,15 +6,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class UserController {
+  private final UserDetailService userDetailService;
   
   @PostMapping("/user")
-  public String userSignIn(@RequestBody UserSignInRequest userSignInRequest){
-    userSignInRequest.getUsername();
-    userSignInRequest.getPassword();
-    return userSignInRequest.getUsername()+" accepted";
+  public UserSignInStatus userSignIn(@RequestBody UserSignInRequest userSignInRequest){
+    return userDetailService
+    .signInUser(userSignInRequest);
   }
 
 }
