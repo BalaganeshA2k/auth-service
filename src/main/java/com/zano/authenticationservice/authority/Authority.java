ba94aa469be24352a5e4c2566a1a0bde8dfbcf50
BalaganeshA2k <balapunch367@gmail.com>
2023-05-26 01:00:22 +0530

    organize imports in UserDetail.java , Authority.java

--- src/main/java/com/zano/authenticationservice/authority/Authority.java
+++ src/main/java/com/zano/authenticationservice/authority/Authority.java
@@ -1,12 +1,12 @@
 package com.zano.authenticationservice.authority;
 
+import static jakarta.persistence.FetchType.LAZY;
+
 import java.util.Set;
 
 import com.zano.authenticationservice.user.UserDetail;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -23,7 +23,7 @@ public class Authority {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
-  @ManyToMany(fetch = FetchType.LAZY,mappedBy = "authorities")
+  @ManyToMany(fetch = LAZY,mappedBy = "authorities")
   private Set<UserDetail> userDetails;
   
 }
--- src/main/java/com/zano/authenticationservice/user/UserDetail.java
+++ src/main/java/com/zano/authenticationservice/user/UserDetail.java
@@ -1,11 +1,13 @@
 package com.zano.authenticationservice.user;
 
+import static jakarta.persistence.CascadeType.MERGE;
+import static jakarta.persistence.CascadeType.PERSIST;
+
 import java.util.HashSet;
 import java.util.Set;
 
 import com.zano.authenticationservice.authority.Authority;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -22,15 +24,20 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data @NoArgsConstructor @Builder @AllArgsConstructor
-@Entity @Table(name = "user_details")
+@Entity
+@Table(name = "user_details")
 public class UserDetail {
-  @Id  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
   @Column(unique = true,nullable = false)
   private String username;
+
   @Column(nullable = false)
   private String password;
-  @ManyToMany(fetch = FetchType.LAZY,cascade = {CascadeType.PERSIST,CascadeType.MERGE})
+
+  @ManyToMany(fetch = FetchType.LAZY,cascade = {PERSIST,MERGE})
   @JoinTable(
     name = "user_authorities",
     joinColumns = @JoinColumn(name = "user_detail_id"),
