7d9e4c26141486c5509f0c406799ce32f9610139
BalaganeshA2k <balapunch367@gmail.com>
2023-05-26 01:02:30 +0530

    rename init package to config

--- src/main/java/com/zano/authenticationservice/config/IntitalApplicationConfiguration.java
+++ src/main/java/com/zano/authenticationservice/config/IntitalApplicationConfiguration.java
@@ -1,4 +1,4 @@
-package com.zano.authenticationservice.init;
+package com.zano.authenticationservice.config;
 
 import static com.zano.authenticationservice.ApplicationRoles.ROLE_ADMIN;
 
@@ -6,12 +6,16 @@ import java.util.Set;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ApplicationEventMulticaster;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 
 import com.zano.authenticationservice.ApplicationRoles;
 import com.zano.authenticationservice.authority.Authority;
 import com.zano.authenticationservice.authority.AuthorityRepository;
-import com.zano.authenticationservice.user.UserDetail;
-import com.zano.authenticationservice.user.UserDetailRepository;
+import com.zano.authenticationservice.user.detail.UserDetail;
+import com.zano.authenticationservice.user.detail.UserDetailRepository;
+
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -26,6 +30,13 @@ public class IntitalApplicationConfiguration {
             authorityRepository.save(defaultUserAuthority());
             userDetailRepository.save(defaultAdminUser());
         };
+    }
+
+    @Bean
+    ApplicationEventMulticaster applicationEventMulticaster() {
+        var multicaster = new SimpleApplicationEventMulticaster();
+        multicaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        return multicaster;
     }
 
     private Authority defaultUserAuthority() {
