1b069735612f25705a58a006c4a2802face5b476
BalaganeshA2k <balapunch367@gmail.com>
2023-05-26 01:00:24 +0530

    handle duplicate user sign-in with UserSignInStatus instead of throwing exception

--- src/main/java/com/zano/authenticationservice/user/UserDetailService.java
+++ src/main/java/com/zano/authenticationservice/user/UserDetailService.java
@@ -8,11 +8,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserDetailService {
   private final UserDetailRepository userDetailRepository;
-  
-  public UserSignInStatus signInUser(UserSignInRequest userSignInRequest){
-    var userDetailsExists =userDetailRepository.existsByUsername(userSignInRequest.getUsername());
-    if(userDetailsExists) throw new DuplicateUserSignInRequestException();
-    var userDetail =userSignInRequest.toUserDetail();
+
+  public UserSignInStatus signInUser(UserSignInRequest userSignInRequest) {
+    var userDetailsExists = userDetailRepository.existsByUsername(userSignInRequest.getUsername());
+    if (userDetailsExists)
+      return UserSignInStatus.FAILED_TO_SIGN_IN_EXISTING_USER;
+    var userDetail = userSignInRequest.toUserDetail();
     userDetailRepository.save(userDetail);
     return UserSignInStatus.SIGN_IN_SUCCESSFULL;
   }
