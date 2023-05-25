ddafa6a18187ed0fc8ed18d4d5ef68ca4a675c52
BalaganeshA2k <balapunch367@gmail.com>
2023-05-26 01:02:13 +0530

    Change Return status to Processing

--- src/main/java/com/zano/authenticationservice/user/UserController.java
+++ src/main/java/com/zano/authenticationservice/user/UserController.java
@@ -1,12 +1,14 @@
 package com.zano.authenticationservice.user;
 
-import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.PROCESSING;
+
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -16,7 +18,7 @@ public class UserController {
   private final UserDetailService userDetailService;
 
   @PostMapping
-  @ResponseStatus(CREATED)
+  @ResponseStatus(PROCESSING)
   public void userSignIn(
       @RequestBody @Validated UserSignInRequest userSignInRequest) {
     userDetailService.signInUser(userSignInRequest);
