498cb562f5873231e19edf92214c1cda125e9dfa
BalaganeshA2k <balapunch367@gmail.com>
2023-05-26 01:02:04 +0530

    Remove Unused ExceptionHandlers; Add Handler for MethodArgumentNotValidException

--- src/main/java/com/zano/authenticationservice/user/UserControllerAdvice.java
+++ src/main/java/com/zano/authenticationservice/user/UserControllerAdvice.java
@@ -1,20 +1,32 @@
 package com.zano.authenticationservice.user;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import com.zano.authenticationservice.ApplicationErrorResponse;
 
 @RestControllerAdvice
 public class UserControllerAdvice {
-    private enum ResponseMessages{
-        USER_ALREADY_EXISTS
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, List<String>>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors().stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(UserAlreadyExistsException.class)
-    @ResponseStatus(value = HttpStatus.CONFLICT)
-    public ApplicationErrorResponse duplicateUserInsertionHandler(UserAlreadyExistsException exception){
-        return new ApplicationErrorResponse(ResponseMessages.USER_ALREADY_EXISTS.name());
+    private Map<String, List<String>> getErrorsMap(List<String> errors) {
+        Map<String, List<String>> errorResponse = new HashMap<>();
+        errorResponse.put("errors", errors);
+        return errorResponse;
     }
 }
