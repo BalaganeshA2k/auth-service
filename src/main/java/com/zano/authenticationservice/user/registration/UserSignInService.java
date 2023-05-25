2c15a87a324bbad512b5411e99a4284ee97074e8
BalaganeshA2k <balapunch367@gmail.com>
2023-05-26 01:03:14 +0530

    user EncodedEmailPasswordGenerator instead of EncodedPasswordGenerator

--- src/main/java/com/zano/authenticationservice/user/registration/UserSignInService.java
+++ src/main/java/com/zano/authenticationservice/user/registration/UserSignInService.java
@@ -3,21 +3,21 @@ package com.zano.authenticationservice.user.registration;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
+import com.zano.authenticationservice.security.EncodedEmailPasswordGenerator;
 import com.zano.authenticationservice.user.detail.UserDetailService;
-import com.zano.authenticationservice.user.verification.EncodedPasswordGenerator;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class UserSignInService {
-    private final EncodedPasswordGenerator passwordGenerator;
+    private final EncodedEmailPasswordGenerator passwordGenerator;
     private final ApplicationEventPublisher applicationEventPublisher;
     private final UserDetailService userDetailService;
 
     public String signInNewUser(UserSignInRequest userSignInRequest) {
         String email = userSignInRequest.email();
-        var password = passwordGenerator.apply(email);
+        var password = passwordGenerator.generate(email);
         var userEmailVerificationEvent = new UserEmailVerificationEvent(email, password);
         applicationEventPublisher.publishEvent(userEmailVerificationEvent);
         userDetailService.saveNewUser(email, password);
