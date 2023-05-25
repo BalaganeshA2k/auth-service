553023e83b3fd2642ade9da387f309e3a17c8679
BalaganeshA2k <balapunch367@gmail.com>
2023-05-26 01:02:09 +0530

    refactor JwtGenerator for reuse of code

--- src/main/java/com/zano/authenticationservice/jwt/JwtGenerator.java
+++ src/main/java/com/zano/authenticationservice/jwt/JwtGenerator.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import com.zano.authenticationservice.user.UserSignInRequest;
+
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
@@ -27,21 +29,24 @@ public class JwtGenerator {
     return generateJwt(new HashMap<>(), userDetails);
   }
 
+  public String generateSignInJwt(UserSignInRequest userSignInRequest) {
+    return buildToken(Map.of(), userSignInRequest.email());
+  }
+
   public String generateJwt(
       Map<String, Object> extraClaims,
       UserDetails userDetails) {
-    return buildToken(extraClaims, userDetails, jwtExpiration);
+    return buildToken(extraClaims, userDetails.getUsername());
   }
 
   private String buildToken(
       Map<String, Object> extraClaims,
-      UserDetails userDetails,
-      long expiration) {
+      String subject) {
     return Jwts.builder()
         .setClaims(extraClaims)
-        .setSubject(userDetails.getUsername())
+        .setSubject(subject)
         .setIssuedAt(new Date(System.currentTimeMillis()))
-        .setExpiration(new Date(System.currentTimeMillis() + expiration))
+        .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
         .setIssuer(issuer)
         .signWith(signKeySupplier.get(), SignatureAlgorithm.HS256)
         .compact();
