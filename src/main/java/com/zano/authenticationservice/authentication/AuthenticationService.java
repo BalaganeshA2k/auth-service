02e2e81d1482a8e195eae8ccd43841876b11ca73
BalaganeshA2k <balapunch367@gmail.com>
2023-06-14 20:28:03 +0530

    persist UserAuthenticationEvent - AuthenticationService.java

--- src/main/java/com/zano/authenticationservice/authentication/AuthenticationService.java
+++ src/main/java/com/zano/authenticationservice/authentication/AuthenticationService.java
@@ -1,8 +1,11 @@
 package com.zano.authenticationservice.authentication;
 
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.zano.authenticationservice.jwt.JwtService;
+import com.zano.authenticationservice.jwt.JwtGenerator.UserAuthenticationEvent;
 import com.zano.authenticationservice.otp.OtpService;
 import com.zano.authenticationservice.otp.exception.EmailAndOtpDoesNotMatchException;
 import com.zano.authenticationservice.otp.exception.OtpExpiredException;
@@ -14,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 public class AuthenticationService {
     private final OtpService otpService;
     private final JwtService jwtService;
+    private final UserAuthenticationRepository userAuthenticationRepository;
 
     public UserAuthentication getUserAuthentication(String emailId, String otp) {
         return switch (otpService.validate(emailId, otp)) {
@@ -22,5 +26,20 @@ public class AuthenticationService {
             case OTP_EXPRIRED -> throw new OtpExpiredException();
             case VALIDATION_SUCCESS -> jwtService.generateAuthenticationToken(emailId);
         };
+    }
+
+    @EventListener
+    @Async
+    public void userAuthenticationGeneratedEvent(UserAuthenticationEvent userAuthenticationGeneratedEvent) {
+        save(userAuthenticationGeneratedEvent);
+    }
+
+    public void save(UserAuthenticationEvent userAuthenticationEvent) {
+        userAuthenticationRepository.save(userAuthenticationEvent);
+    }
+
+    public boolean isUserAuthenticationExists(String email) {
+        return userAuthenticationRepository.existsById(email);
+
     }
 }
