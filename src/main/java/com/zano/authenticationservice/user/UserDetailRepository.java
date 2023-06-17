8f25fab5d309c1e6aad1b2539905152749b00230
BalaganeshA2k <balapunch367@gmail.com>
2023-06-17 23:24:13 +0530

    add: findAuthoritiesByUserEmail to UserDetailRepository

--- src/main/java/com/zano/authenticationservice/user/UserDetailRepository.java
+++ src/main/java/com/zano/authenticationservice/user/UserDetailRepository.java
@@ -1,12 +1,19 @@
 package com.zano.authenticationservice.user;
 
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.zano.authenticationservice.user.authority.UserAuthority;
 
 public interface UserDetailRepository extends JpaRepository<UserDetail, String> {
   Optional<UserDetail> findByUserEmail(String userEmail);
 
   boolean existsByUserEmail(String userEmail);
+
+  @Query("SELECT ua FROM UserDetail ud JOIN ud.authorities ua WHERE ud.userEmail = :userEmail")
+  Set<UserAuthority> findAuthoritiesByUserEmail(String userEmail);
 
 }
